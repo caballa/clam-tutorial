671e16b85fdd351f5408bdcaa92536678f3b8d94
caballa <navasjorgea@gmail.com>
2021-02-15 00:30:08 -0600

    use of ClamQueryAPI

--- api-example/myanalyzer.cpp
+++ api-example/myanalyzer.cpp
@@ -30,8 +30,10 @@ int main(int argc, char *argv[]) {
   
   std::ifstream source;
   source.open(argv[1]);
-  
+
+  //////////////////////////////////////  
   // Get module from LLVM file
+  //////////////////////////////////////  
   LLVMContext Context;
   SMDiagnostic Err;
   std::unique_ptr<Module> module = parseIRFile(argv[1], Err, Context);
@@ -45,7 +47,9 @@ int main(int argc, char *argv[]) {
   Triple triple(tripleT);
   TargetLibraryInfoWrapperPass  TLIW(triple);  
 
+  //////////////////////////////////////
   // Run seadsa -- pointer analysis
+  //////////////////////////////////////  
   CallGraph cg(*module);
   seadsa::AllocWrapInfo allocWrapInfo(&TLIW);
   allocWrapInfo.initialize(*module, nullptr);
@@ -53,16 +57,18 @@ int main(int argc, char *argv[]) {
   dsaLibFuncInfo.initialize(*module);
   std::unique_ptr<HeapAbstraction> mem(new SeaDsaHeapAbstraction(
 		*module, cg, TLIW, allocWrapInfo, dsaLibFuncInfo, true));
-  
-  // Clam
 
+  //////////////////////////////////////  
+  // Clam
+  //////////////////////////////////////
+  
   /// Translation from LLVM to CrabIR
   CrabBuilderParams cparams;
   cparams.setPrecision(clam::CrabBuilderPrecision::MEM);
   cparams.print_cfg = true;
   CrabBuilderManager man(cparams, TLIW, std::move(mem));
   
-  /// Set analysis parameters
+  /// Set Crab parameters
   AnalysisParams aparams;
   aparams.dom = CrabDomain::INTERVALS;
   aparams.run_inter = true;
@@ -70,21 +76,35 @@ int main(int argc, char *argv[]) {
   
   /// Create an inter-analysis instance 
   InterGlobalClam ga(*module, man);
-  /// Run the analysis
+  /// Run the Crab analysis
   ClamGlobalAnalysis::abs_dom_map_t assumptions;
   ga.analyze(aparams, assumptions);
   
   /// Print program invariants inferred by Clam
+  llvm::errs() << "===Invariants at the entry of each block===\n";
   for (auto &f: *module) {
     for (auto &b: f) {
       llvm::Optional<clam_abstract_domain> dom = ga.getPre(&b);
       if (dom.hasValue()) {
-	crab::outs() << f.getName() << "#" << b.getName() << ":  " << dom.getValue() << "\n";
+	crab::outs() << f.getName() << "#" << b.getName() << ":\n  " << dom.getValue() << "\n";
+      }
+    }
+  }
+  llvm::errs() << "===Ranges for each LoadInst===\n";
+  /// Ask for the ranges of each LoadInst
+  for (auto &f: *module) {
+    for (auto &b: f) {
+      for (auto &i: b) {
+	if (isa<LoadInst>(i)) {
+	  auto rangeVal = ga.range(i);
+	  llvm::errs() << "Range for " << i << " = [" << rangeVal.first << "," << rangeVal.second << "]\n";
+	}
       }
     }
   }
   
   /// Print results about assertion checks
+  llvm::errs() << "===Assertion checks ===\n";
   ga.getChecksDB().write(crab::outs());
   
   return 0;
