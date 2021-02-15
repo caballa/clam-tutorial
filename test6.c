b81d6696747e2b88c511854636c9b758a8aed2f9
caballa <navasjorgea@gmail.com>
2021-02-15 00:21:05 -0600

    Fix test6

--- test6.c
+++ test6.c
@@ -1,5 +1,5 @@
-// RUN: %clam -g -O0 --crab-inter --crab-dom=int --crab-track=mem --promote-malloc=false --crab-check=uaf "%s" 2>&1 | OutputCheck %s
-// CHECK: ^0  Number of total warning checks$
+// RUN: %clam -g -O0 --crab-inter --crab-dom=int --crab-track=mem --crab-heap-analysis=cs-sea-dsa --promote-malloc=false --crab-check=uaf "%s" 2>&1 | OutputCheck %s
+// CHECK: ^ 0  Number of total warning checks$
 
 #include <stdlib.h>
 
@@ -7,36 +7,49 @@ extern int int_nd(void);
 extern void __CRAB_assert(int);
 extern void __CRAB_assume(int);
 
-
 struct s {
-  int f1;
-  int f2;
-  int f3;
+  int* f1;
+  int* f2;
 };
 
+void del(struct s* p) {
+  *(p->f1) = int_nd();
+  free(p->f1);
+}
 
-void init(struct s* p) {
-  // x is allocated here but it doesn't escape
+void initX(struct s* p) {
   int *x = (int*) malloc(sizeof(int));
+  __CRAB_assume(x > 0);    
   if (int_nd()) {
     *x = 5;
   } else {
     *x = 7;
   }
-  p->f1 = *x;
-  p->f2 = int_nd();
-  p->f3 = *x;
-  free(x);
-  // inject bug
-  //free(&(p->f3));
+  int *y = (int*) malloc(sizeof(int));
+  __CRAB_assume(y > 0);  
+  *y = int_nd();
+  
+  p->f1 = y;
+  p->f2 = x;
+}
+
+void init(struct s* p) {
+  initX(p);
+}
+
+void check(struct s* p) {
+  __CRAB_assert(*(p->f2) >= 5);
 }
 
 int main () {
-  // a memory block is allocated and passed to callee
   struct s *p = (struct s*) malloc(sizeof(struct s));
-  __CRAB_assume(p > 0);  
+  __CRAB_assume(p > 0);
+
   init(p);
-  int x = p->f3;
-  __CRAB_assert(x >= 5);
+
+  del(p);
+
+  check(p);
+
   return 0;
 }
